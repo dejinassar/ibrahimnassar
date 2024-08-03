8e43c3117db9b7dc8f1e75230b568b224da4fc2e
dejinassar <dejinassar@gmail.com>
2024-08-03 16:15:10 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function openMenu() {
+    document.getElementById('sideMenu').classList.remove('hidden');
+}
+
+function closeMenu() {
+    document.getElementById('sideMenu').classList.add('hidden');
+}
+function toggleDarkMode() {
+    document.body.classList.toggle('dark');
+}
+
+function openMenu() {
+    document.getElementById('sideMenu').classList.remove('hidden');
+}
+
+function closeMenu() {
+    document.getElementById('sideMenu').classList.add('hidden');
+}
